e2f48cf03882f27ee7dea7b3dfb7f7d45d1bf1c8
samicrusader <hi@samicrusader.me>
2024-08-25 18:43:29 -0400

    fix: scheduler conflict?

--- init.php
+++ init.php
@@ -1,10 +1,10 @@
 <?php
 
 $req = new rXMLRPCRequest( array(
-	$theSettings->getOnInsertCommand(array('tadd_trackers3'.User::getUser(), getCmd('d.set_custom4').'=$'.getCmd('cat').'=$'.getCmd('d.get_state='))),
+	$theSettings->getOnInsertCommand(array('tadd_trackers3'.User::getUser(), getCmd('d.set_custom6').'=$'.getCmd('cat').'=$'.getCmd('d.get_state='))),
 	$theSettings->getOnInsertCommand(array('tadd_trackers4'.User::getUser(),
-		getCmd('branch').'=$'.getCmd('not').'=$'.getCmd('d.get_custom3').'=,"'.getCmd('cat').'=$'.getCmd('d.stop').'=,\"$'.
-			getCmd('execute').'={sh,'.$rootPath.'/plugins/unretracker/run.sh'.','.Utility::getPHP().',$'.getCmd('d.get_hash').'=,'.User::getUser().'}\"" ; '.getCmd('d.set_custom3=')))));
+		getCmd('branch').'=$'.getCmd('not').'=$'.getCmd('d.get_custom5').'=,"'.getCmd('cat').'=$'.getCmd('d.stop').'=,\"$'.
+			getCmd('execute').'={sh,'.$rootPath.'/plugins/unretracker/run.sh'.','.Utility::getPHP().',$'.getCmd('d.get_hash').'=,'.User::getUser().'}\"" ; '.getCmd('d.set_custom5=')))));
 if($req->run() && !$req->fault)
 {
 	$theSettings->registerPlugin($plugin["name"],$pInfo["perms"]);
